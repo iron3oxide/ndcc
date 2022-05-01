033f3e90621dfd08aba01eb4dfa0b79dd5a7c4a9
iron3oxide <jason.hottelet@gmx.de>
2022-05-02 01:08:55 +0200

    refactor(input, validate): Use getters instead of global variables
    Guess I was too lazy before.

--- ndcc/input.py
+++ ndcc/input.py
@@ -19,7 +19,7 @@ def get_selected_charts() -> list[str]:
 def get_collection_count() -> int:
     selected_number_of_pick_collections: int = int(input_dialog(
         text="Enter the number of pick collections to compare: ",
-        validator=validate.collection_num_validator
+        validator=validate.get_collection_count_validator()
     ).run())
     return selected_number_of_pick_collections
 
@@ -27,6 +27,7 @@ def get_collection_count() -> int:
 def get_collection_name(collection_number: int) -> str:
     collection_name: str = input_dialog(
         text=f"Enter a name/identifier for collection {collection_number}, e.g. 'Vikings receive': ",
+        validator=validate.get_collection_name_validator()
     ).run()
     return collection_name
 
@@ -34,7 +35,7 @@ def get_collection_name(collection_number: int) -> str:
 def get_picks(collection_number: int) -> list[int]:
     picks_raw: list[str] = input_dialog(
         text=f"Enter the picks in collection {collection_number}, separated by spaces: ",
-        validator=validate.pick_validator
+        validator=validate.get_pick_validator()
     ).run().split()
     picks: list[int] = [int(i) for i in picks_raw]
     return picks
--- ndcc/validate.py
+++ ndcc/validate.py
@@ -1,7 +1,7 @@
 from prompt_toolkit.validation import Validator
 
 
-def _validate_collection_number(text: str) -> bool:
+def _validate_collection_count(text: str) -> bool:
     return text.isdigit() and 1 < int(text) < 32
 
 
@@ -16,20 +16,28 @@ def _validate_collection_name(text: str) -> bool:
     return bool(text)
 
 
-collection_num_validator: Validator = Validator.from_callable(
-    _validate_collection_number,
-    "Number of collections must be between 2 and 32",
-    move_cursor_to_end=True
-)
+def get_collection_count_validator() -> Validator:
+    collection_count_validator: Validator = Validator.from_callable(
+        _validate_collection_count,
+        "Number of collections must be between 2 and 32",
+        move_cursor_to_end=True
+    )
+    return collection_count_validator
 
-pick_validator: Validator = Validator.from_callable(
-    _validate_picks,
-    "One or more picks is invalid. Picks must be ints between 1 and 270.",
-    move_cursor_to_end=True
-)
 
-collection_name_validator: Validator = Validator.from_callable(
-    _validate_collection_name,
-    "Collection name/identifier cannot be empty",
-    move_cursor_to_end=True
-)
+def get_pick_validator() -> Validator:
+    pick_validator: Validator = Validator.from_callable(
+        _validate_picks,
+        "One or more picks is invalid. Picks must be ints between 1 and 270.",
+        move_cursor_to_end=True
+    )
+    return pick_validator
+
+
+def get_collection_name_validator() -> Validator:
+    collection_name_validator: Validator = Validator.from_callable(
+        _validate_collection_name,
+        "Collection name/identifier cannot be empty",
+        move_cursor_to_end=True
+    )
+    return collection_name_validator
